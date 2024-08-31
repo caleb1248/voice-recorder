dc0255c3f8e31c28cfcf6c32f23e899c3cb2a804
caleb1248 <28stu647@lexingtonma.org>
2024-08-31 12:31:49 -0400

    add video recording support

--- src/main.js
+++ src/main.js
@@ -4,26 +4,58 @@ import './style.css';
 // Write TypeScript code!
 const appDiv = document.getElementById('controls');
 const startStopButton = document.getElementById('startStop');
+const useVideo = location.search.includes('video');
+const countdownElement = document.getElementById('countdown');
+
+document.querySelector('#toggleVideo').onclick = () => {
+  if (useVideo) {
+    location.search = '';
+  } else location.search = '?video';
+};
+if (useVideo) document.querySelector('#buttons').classList.add('vertical');
 
 let media;
 let recorder;
 
-const audioElement = document.createElement('audio');
-audioElement.controls = true;
+const mediaPlayer = document.createElement(useVideo ? 'video' : 'audio');
+mediaPlayer.controls = true;
 
 let url;
+
+function wait1sec() {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+async function countdown() {
+  countdownElement.classList.add('visible');
+  countdownElement.textContent = '3';
+  await wait1sec();
+  countdownElement.textContent = '2';
+  await wait1sec();
+  countdownElement.textContent = '1';
+  await wait1sec();
+  countdownElement.textContent = '';
+  countdownElement.classList.remove('visible');
+}
 
 async function activateMedia() {
   media = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: false,
+    video: useVideo,
   });
+
+  if (useVideo) {
+    mediaPlayer.width = 320;
+    mediaPlayer.height = 240;
+    mediaPlayer.srcObject = media;
+    mediaPlayer.play();
+  }
 
   recorder = new MediaRecorder(media);
   recorder.addEventListener('dataavailable', (e) => {
     if (url) URL.revokeObjectURL(url);
-    url = audioElement.src = URL.createObjectURL(e.data);
-    appDiv.appendChild(audioElement);
+    url = mediaPlayer.src = URL.createObjectURL(e.data);
+    appDiv.appendChild(mediaPlayer);
   });
 }
 
@@ -35,10 +67,20 @@ async function startRecording() {
     return;
   }
 
-  if (recorder.state !== 'recording') recorder.start();
   startStopButton.textContent = 'Stop';
   startStopButton.onclick = stopRecording;
-  audioElement.pause();
+
+  if (!useVideo) mediaPlayer.pause();
+  if (useVideo) {
+    mediaPlayer.muted = true;
+    mediaPlayer.play();
+    mediaPlayer.controls = false;
+    appDiv.appendChild(mediaPlayer);
+
+    await countdown();
+  }
+
+  if (recorder.state !== 'recording') recorder.start();
 }
 
 async function stopRecording() {
@@ -46,6 +88,10 @@ async function stopRecording() {
   if (media) media.getTracks().forEach((track) => track.stop());
   startStopButton.textContent = 'Record';
   startStopButton.onclick = startRecording;
+  mediaPlayer.pause();
+  mediaPlayer.srcObject = null;
+  mediaPlayer.muted = false;
+  mediaPlayer.controls = true;
 }
 
 window.startRecording = startRecording;
